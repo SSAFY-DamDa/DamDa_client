b21e6d971312f0f64cc9a89f2f8a79f5fde22cfd
lha0 <lha0@unist.ac.kr>
2024-11-21 17:45:47 +0900

    feat: makeself 오른쪽 사이드바 제작

--- src/components/makeself/aside/rightaside/MakeTripListItem.vue
+++ src/components/makeself/aside/rightaside/MakeTripListItem.vue
@@ -1,7 +1,37 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const isOpen = ref(false);
+
+const toggleContent = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
 
 <template>
-  <li>아이템</li>
+  <li class="make-trip-list-item" @click="toggleContent">
+    <div class="item-title">{{ day }}일차</div>
+    <div v-if="isOpen" class="item-content">내용</div>
+  </li>
 </template>
 
-<style scoped></style>
+<style scoped>
+.make-trip-list-item {
+  width: 90%;
+  padding-left: 10px;
+  border: 1px solid #c9c9c9;
+  border-radius: 5px;
+  line-height: 40px;
+  color: #787878;
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.item-title {
+  height: 40px;
+}
+
+.item-content {
+  margin-top: 5px;
+  color: #555;
+}
+</style>
--- src/stores/journey.js
+++ src/stores/journey.js
@@ -1,21 +1,45 @@
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { defineStore } from "pinia";
 
 export const useJourneyStore = defineStore("journey", () => {
   const journeyList = ref([{ id: 12 }]);
-  const journeyDetail = ref({});
+  const journeyDetail = ref({
+    title: "",
+    user_id: 0,
+    sido_code: 0,
+    gugun_code: 0,
+    content_type_id: 0,
+    start_date: "",
+    end_date: "",
+    personnel: 0,
+    color: "",
+  });
   const journeyPeriod = ref(0);
-
-  const getJourneyList = computed(() => journeyList.value);
-  const getJourneyDetail = computed(() => journeyDetail.value);
-  const getJourneyPeriod = computed(() => journeyPeriod.value);
 
   const setJourneyList = (_journeyList) => {
     journeyList.value = _journeyList;
   };
 
-  const setJourneyDetail = (_journeyDetail) => {
-    journeyDetail.value = _journeyDetail;
+  const setJourneyDetail = (
+    _title,
+    _user_id,
+    _sido_code,
+    _gugun_code,
+    _content_type_id,
+    _start_date,
+    _end_date,
+    _personnel,
+    _color
+  ) => {
+    journeyDetail.value.title = _title;
+    journeyDetail.value.user_id = _user_id;
+    journeyDetail.value.sido_code = _sido_code;
+    journeyDetail.value.gugun_code = _gugun_code;
+    journeyDetail.value.content_type_id = _content_type_id;
+    journeyDetail.value.start_date = _start_date;
+    journeyDetail.value.end_date = _end_date;
+    journeyDetail.value.personnel = _personnel;
+    journeyDetail.value.color = _color;
   };
 
   const setJourneyPeriod = (_journeyPeriod) => {
@@ -23,9 +47,8 @@ export const useJourneyStore = defineStore("journey", () => {
   };
 
   return {
-    getJourneyList,
-    getJourneyDetail,
-    getJourneyPeriod,
+    journeyDetail,
+    journeyPeriod,
     setJourneyList,
     setJourneyDetail,
     setJourneyPeriod,
