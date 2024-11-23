076a2439db02fcce67960fe2bac8d5c0ca90fe62
PoeySK <piz115@naver.com>
2024-11-23 10:06:32 +0900

    refactor: axios 한 파일에서 관리
    api 요청구조화

--- src/api/journey.js
+++ src/api/journey.js
@@ -1,18 +1,19 @@
-import { JourneyAxios } from "@/utils/http-journey";
+import { Axios } from "@/utils/client";
 
+const JOURNEY = "journey";
 const getJourneyList = async (suc, err) => {
-  return await JourneyAxios().get("/list").then(suc).catch(err);
+  return await Axios(JOURNEY).get("/list").then(suc).catch(err);
 };
 
 const getJourney = async (param, suc, err) => {
-  return await JourneyAxios()
+  return await Axios(JOURNEY)
     .get("", { params: { userId: param } })
     .then(suc)
     .catch(err);
 };
 
 const getDetailJourney = async (param, suc, err) => {
-  return await JourneyAxios().get(`/detail/${param}`).then(suc).catch(err);
+  return await Axios(JOURNEY).get(`/detail/${param}`).then(suc).catch(err);
 };
 
 export { getJourneyList, getJourney, getDetailJourney };
