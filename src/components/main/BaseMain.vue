afa0538fa3fa0fcddc6cd398315d2cd21595153d
PoeySK <piz115@naver.com>
2024-11-26 13:28:56 +0900

    style: 스크롤 기준 헤더 변경
    헤더 색상 변경

--- src/components/main/BaseMain.vue
+++ src/components/main/BaseMain.vue
@@ -3,8 +3,10 @@ import { onMounted, onUnmounted, ref } from "vue";
 import mainBg from "@/assets/imgs/main_bg.jpg";
 import { useRouter } from "vue-router";
 import ReviewList from "../review/ReviewList.vue";
+import { useLogoStore } from "@/stores/logo";
 
 const router = useRouter();
+const logoStore = useLogoStore();
 
 const subContent = ref(null);
 const mainContent = ref(null);
@@ -15,8 +17,10 @@ const handleScroll = () => {
   textNameElements.forEach((element) => {
     if (scrollPosition >= 3900) {
       element.style.color = "black";
+      logoStore.changeLogo("basic");
     } else {
       element.style.color = "white";
+      logoStore.changeLogo("white");
     }
   });
   const contentBtnElements = document.querySelectorAll(".content-btn");
@@ -37,45 +41,45 @@ const checkScroll = () => {
   const mainWrapperHeight =
     document.getElementById("main-wrapper").offsetHeight;
 
-  // main-wrapper가 끝나는 지점을 넘어갔는지 확인
   if (scrollPosition >= mainWrapperHeight - windowHeight) {
     subContent.value.style.opacity = 0;
     mainContent.value.style.opacity = 0;
     return;
   }
 
-  // opacity 계산 구간 조정
   const subOpacity = Math.max(1 - scrollPosition / windowHeight, 0);
-  // opacity 시작점을 늦추고 끝점을 연장 (0.3 ~ 0.8 구간에서 변화)
   const mainOpacity = Math.max(
     Math.min((scrollPosition - windowHeight * 0.3) / (windowHeight * 0.5), 1),
     0
   );
 
-  // AI가 짜주는 여행만 scale 계산 (1에서 1.5까지)
-  const mainScale = 1 + (scrollPosition / windowHeight) * 0.3; // 1에서 1.5로
+  const mainScale = 1 + (scrollPosition / windowHeight) * 0.3;
 
   subContent.value.style.opacity = subOpacity;
   mainContent.value.style.opacity = mainOpacity;
 
-  // 내가 짜보는 여행은 크기 고정
   subContent.value.style.transform = `translate(-50%, -50%)`;
-  // AI가 짜주는 여행만 확대
   mainContent.value.style.transform = `translate(-50%, -50%) scale(${mainScale})`;
 };
 
 onMounted(() => {
+  logoStore.changeLogo("white");
   const textNameElements = document.querySelectorAll(".text-name");
+  const contentBtnElements = document.querySelectorAll(".content-btn");
   textNameElements.forEach((element) => {
     element.style.color = "white";
   });
+  contentBtnElements.forEach((element) => {
+    element.style.display = "flex";
+  });
   window.addEventListener("scroll", checkScroll);
   window.addEventListener("scroll", handleScroll);
-  // 초기 상태 설정
+
   checkScroll();
 });
 
 onUnmounted(() => {
+  logoStore.changeLogo("basic");
   console.log("main unmounted");
   window.removeEventListener("scroll", handleScroll);
   const textNameElements = document.querySelectorAll(".text-name");
@@ -85,7 +89,7 @@ onUnmounted(() => {
       element.style.color = "black";
     });
     contentBtnElements.forEach((element) => {
-      element.style.display = "flex";
+      element.style.display = "none";
     });
   };
 
@@ -98,11 +102,13 @@ onUnmounted(() => {
 <template>
   <img :src="mainBg" alt="main-bg" id="main-bg" />
   <div id="main-wrapper">
-    <div ref="subContent" class="content sub-content">내가 짜보는 여행</div>
-    <div ref="mainContent" class="content main-content">AI가 짜주는 여행</div>
+    <div ref="subContent" class="content sub-content">내가 만드는 추억</div>
+    <div ref="mainContent" class="content main-content">
+      AI가 만들어주는 추억
+    </div>
   </div>
   <div class="content-box">
-    <div id="map-box" data-aos="fade-down-right" data-aos-duration="1000">
+    <div id="map-box" data-aos="fade-down" data-aos-duration="1000">
       <div class="content-wrapper">
         <img
           src="@/assets/imgs/Mockup_phone.png"
@@ -121,7 +127,7 @@ onUnmounted(() => {
         </div>
       </div>
     </div>
-    <div id="ai-box" data-aos="fade-down-left" data-aos-duration="1000">
+    <div id="ai-box" data-aos="fade-down" data-aos-duration="1000">
       <div class="content-wrapper">
         <div class="text-container" id="ai-text-container">
           <p>AI 추천</p>
@@ -141,7 +147,7 @@ onUnmounted(() => {
       </div>
       <ReviewList />
     </div>
-    <div id="self-box" data-aos="fade-down-right" data-aos-duration="1000">
+    <div id="self-box" data-aos="fade-down" data-aos-duration="1000">
       <div class="content-wrapper">
         <img
           src="@/assets/imgs/Mockup_Ipad.png"
@@ -167,6 +173,7 @@ onUnmounted(() => {
 #main-wrapper {
   top: -60px;
   position: relative;
+  width: 100%;
   height: 4000px;
   background-color: #000000;
   display: flex;
@@ -177,13 +184,13 @@ onUnmounted(() => {
 }
 
 #main-bg {
-  position: fixed; /* 화면에 고정 */
+  position: fixed;
   top: 50%;
   left: 50%;
   width: 100%;
-  max-width: 1500px; /* 최대 너비를 1500px로 제한 */
-  transform: translate(-50%, -50%); /* 화면 중앙에 배치 */
-  z-index: -1; /* 콘텐츠 뒤에 위치 */
+  height: 100%;
+  transform: translate(-50%, -50%);
+  z-index: -1;
 }
 
 .content {
@@ -195,7 +202,7 @@ onUnmounted(() => {
   justify-content: center;
   align-items: center;
   color: #ffffff;
-  transition: all 0.3s ease; /* 부드러운 전환 효과 추가 */
+  transition: all 0.3s ease;
   z-index: 1;
   font-size: 4rem;
   font-weight: bold;
@@ -209,26 +216,26 @@ onUnmounted(() => {
 
 .sub-content {
   opacity: 1;
-  transform: translate(-50%, -50%); /* 크기 변화 없음 */
+  transform: translate(-50%, -50%);
 }
 
 .main-content {
   opacity: 0;
-  transform: translate(-50%, -50%) scale(1); /* 초기 크기 1로 설정 */
+  transform: translate(-50%, -50%) scale(1);
 }
 
 .content-wrapper {
   height: 80%;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
   gap: 300px;
 }
 
 #map-box,
 #self-box {
-  width: 80%;
-  height: 100vh;
+  width: 70%;
+  height: 90vh;
   margin: 0 auto;
   display: flex;
   align-items: center;
@@ -236,9 +243,10 @@ onUnmounted(() => {
 }
 
 #ai-box {
-  width: 80%;
+  width: 70%;
   height: 90vh;
   display: flex;
+  margin: 0 auto;
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -250,7 +258,7 @@ onUnmounted(() => {
 }
 
 .text-container {
-  width: 50%;
+  width: 100%;
   height: 50%;
   display: flex;
   flex-direction: column;
