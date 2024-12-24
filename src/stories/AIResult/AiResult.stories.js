97bf09a7db400d6c39df0df90e0ff83a11849831
PoeySK <piz115@naver.com>
2024-12-24 21:54:39 +0900

    chore: title을 Pascal 형식으로 변경

--- src/stories/AIResult/AiResult.stories.js
+++ src/stories/AIResult/AiResult.stories.js
@@ -4,7 +4,7 @@ import LoadingAIResult from "./LoadingAIResult.vue";
  * AI 계획 기능의 모든 절차를 수행하면 최종적으로 볼 수 있는 결과 페이지 입니다.
  */
 export default {
-  title: "page/makeai",
+  title: "page/MakeAI",
   tags: ["autodocs"],
 };
 
